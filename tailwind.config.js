909d283ac893d52fd0220f44b8246078ca028471
ruhulapsis <ruhul.amin@apsissolutions.com>
2024-01-26 22:02:39 +0600

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+    },
   },
   plugins: [],
-}
-
+};
